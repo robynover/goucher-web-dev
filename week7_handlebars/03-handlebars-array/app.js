069ab96ed63027d71a26c4132dd04154229bd1d1
Robyn <robynover@gmail.com>
2016-04-01 17:36:23 -0400

    Added numbers to handlebars projects dir names. New handlebars example

---
+++ week7_handlebars/03-handlebars-array/app.js
@@ -0,0 +1,72 @@
+// bring in Express
+var express = require('express');
+var app = express();
+
+// set up the Handlebars view engine
+var handlebars = require('express-handlebars').create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+// tell express to find static files in the "public directory"
+app.use(express.static('public'));
+
+// --- routes --- //
+app.get("/",function(req,res){
+	// An object to hold the data that will get fed into the template.
+	// Note! the property names here must match the variable names in handlebars
+	var data = {
+		 title: "Welcome to My Site",
+		 content: "I collect things. I know some people."
+	};
+	// Render the template using this data
+	res.render("standard", data);
+});
+
+app.get("/collection",function(req,res){
+	// data object -- contains an array
+	var data = {
+		title: "Things I Collect",
+		things: [
+			'stamps',
+			'legos',
+			'dolls',
+			'paperclips',
+			't-shirts',
+			'postcards',
+			'coins',
+			'Pokemon',
+			'dinosaurs'
+		]
+	};
+	res.render("things", data);
+});
+
+app.get("/contacts", function(req,res){
+	// this data object contains an array of objects
+	var data = {
+		title: "My Contacts",
+		contacts: [
+			{
+				name: 'Fred',
+				email: 'fred@flintsone.com',
+				photo: 'fred.png'
+			},
+			{
+				name: 'Barney',
+				email: 'barney@rubble.com',
+				photo: 'barney.png'
+			},
+			{
+				name: 'Wilma',
+				email: 'wilma@flintsone.com',
+				photo: 'wilma.png'
+			}
+		]
+	};
+	res.render("people",data);
+});
+
+// start the server
+app.listen(3000, function(){
+	console.log( 'Express started on http://localhost:3000; press Ctrl-C to terminate.' );
+});
